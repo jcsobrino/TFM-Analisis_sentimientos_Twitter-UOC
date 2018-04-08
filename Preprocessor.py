a859181cbfb3d293d2559fdb767b102763352d6f
jcsobrino <sobrinosande@gmail.com>
2018-04-08 20:29:18 +0200

    Remove diacritical vowels

--- Preprocessor.py
+++ Preprocessor.py
@@ -6,12 +6,12 @@ from sklearn.feature_extraction.text import strip_accents_unicode
 class Preprocessor:
 
     _stemmer = SnowballStemmer('spanish')
+    _diacritical_vowels = [('á','a'), ('é','e'), ('í','i'), ('ó','o'), ('ú','u'), ('ü','u')]
     _slang = [('d','de'), ('[qk]','que'), ('xo','pero'), ('xa', 'para'), ('[xp]q','porque'),('es[qk]', 'es que'),
               ('fvr','favor'),('(xfa|xf|pf|plis|pls|porfa)', 'por favor'), ('dnd','donde'), ('lol', '_laugh'), ('tb', 'también'),
               ('(tq|tk)', 'te quiero'), ('(tqm|tkm)', 'te quiero mucho'), ('x','por')]
 
-    def __init__(self, strip_accents=True, twitter_symbols=None, stemming=False):
-        self.strip_accents = strip_accents
+    def __init__(self, twitter_symbols=None, stemming=False):
         self.twitter_symbols = twitter_symbols
         self.stemming = stemming
 
@@ -20,6 +20,9 @@ class Preprocessor:
         message = message.lower()
         # remove numbers, carriage returns and retweet old-style method
         message = re.sub(r'(\d+|\n|\brt\b)', '', message)
+        # remove vowels with diacritical marks
+        for s,t in self._diacritical_vowels:
+            message = re.sub(r'{0}'.format(s), t, message)
         # remove repeated characters
         message = re.sub(r'([^clnr])\1+', r'\1', message)
         message = re.sub(r'([clnr])\1{2,}', r'\1\1', message)
@@ -28,10 +31,6 @@ class Preprocessor:
         # normalized slang
         for s,t in self._slang:
             message = re.sub(r'\b{0}\b'.format(s), t, message)
-
-        if self.strip_accents:
-            # remove accents
-            message = strip_accents_unicode(message)
 
         if self.twitter_symbols == 'remove':
             # remove mentions, hashtags and urls
@@ -48,7 +47,7 @@ class Preprocessor:
         return message
 
     def __repr__(self):
-        return "Preprocessor([strip_accents={0}, twitter_symbols={1}, stemming={2}])".format(self.strip_accents, self.twitter_symbols, self.stemming)
+        return "Preprocessor([twitter_symbols={0}, stemming={1}])".format(self.twitter_symbols, self.stemming)
 
     def __str__(self):
-        return "Preprocessor([strip_accents={0}, twitter_symbols={1}, stemming={2}])".format(self.strip_accents, self.twitter_symbols, self.stemming)
+        return "Preprocessor([twitter_symbols={0}, stemming={1}])".format(self.twitter_symbols, self.stemming)
