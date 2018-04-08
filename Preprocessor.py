7a95b003c7ba466fb13d292440e1051002c88df2
jcsobrino <sobrinosande@gmail.com>
2018-04-08 11:18:08 +0200

    Added slang

--- Preprocessor.py
+++ Preprocessor.py
@@ -6,6 +6,9 @@ from sklearn.feature_extraction.text import strip_accents_unicode
 class Preprocessor:
 
     _stemmer = SnowballStemmer('spanish')
+    _slang = [('d','de'), ('[qk]','que'), ('xo','pero'), ('xa', 'para'), ('[xp]q','porque'),('es[qk]', 'es que'),
+              ('fvr','favor'),('(xfa|xf|pf|plis|pls|porfa)', 'por favor'), ('dnd','donde'), ('lol', '_laugh'), ('tb', 'también'),
+              ('(tq|tk)', 'te quiero'), ('(tqm|tkm)', 'te quiero mucho'), ('x','por')]
 
     def __init__(self, strip_accents=True, twitter_symbols='normalize', stemming=False):
         self.strip_accents = strip_accents
@@ -17,6 +20,14 @@ class Preprocessor:
         message = message.lower()
         # remove numbers, carriage returns and retweet old-style method
         message = re.sub(r'(\d+|\n|\brt\b)', '', message)
+        # remove repeated characters
+        message = re.sub(r'([^clnr])\1+', r'\1', message)
+        message = re.sub(r'([clnr])\1{3,}', r'\1\1', message)
+        # normalized laughs
+        message = re.sub(r'\b(?=\w*[j])[aeiouj]{4,}\b', '_laugh', message)
+        # normalized slang
+        for s,t in self._slang:
+            message = re.sub(r'\b{0}\b'.format(s), t, message)
 
         if self.strip_accents:
             # remove accents
@@ -24,16 +35,12 @@ class Preprocessor:
 
         if self.twitter_symbols == 'remove':
             # remove mentions, hashtags and urls
-            message = re.sub(r'(@|#|https?:)\S+', '', message)
+            message = re.sub(r'(@|#|htt?ps?:)\S+', '', message)
         elif self.twitter_symbols == 'normalize':
             # normalize mentions, hashtags and urls
             message = re.sub(r'@\S+', '_mention', message)
             message = re.sub(r'#\S+', '_hashtag', message)
-            message = re.sub(r'https?:\S+', '_url', message)
-
-        # remove repeated characters
-        message = re.sub(r'([^clnr])\1+', r'\1', message)
-        message = re.sub(r'([clnr])\1{3,}', r'\1\1', message)
+            message = re.sub(r'htt?ps?:\S+', '_url', message)
 
         if self.stemming:
             message = ' '.join(self._stemmer.stem(w) for w in message.split())
