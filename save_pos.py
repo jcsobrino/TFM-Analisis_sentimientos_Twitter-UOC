37134b70174a3dd0359e2e47b0770f1b03627173
jcsobrino <sobrinosande@gmail.com>
2018-04-13 21:15:26 +0200

    POS with TweetTokenizer

--- save_pos.py
+++ save_pos.py
@@ -1,4 +1,8 @@
 import os
+
+import re
+
+from nltk import TweetTokenizer
 from nltk.tag.stanford import StanfordPOSTagger as POS_Tag
 from baseline_model import read_corpus
 
@@ -13,9 +17,12 @@ message_test, label_test = read_corpus("datasets/test_dataset_30.csv")
 st = POS_Tag(model_filename=path_to_model, path_to_jar=path_to_jar)
 
 def save_pos(data, filename):
+    t = TweetTokenizer()
     with open(filename, "a", encoding="utf-8") as afile:
         for d in data:
-            afile.write(repr(st.tag([d])))
+            d = re.sub(r'([^clnr])\1+', r'\1', d)
+            d = re.sub(r'([clnr])\1{2,}', r'\1\1', d)
+            afile.write(repr(st.tag(t.tokenize(d))))
             afile.write('\n')
 
 def read_pos(filename):
@@ -25,7 +32,6 @@ def read_pos(filename):
             data.append(eval(line))
     return dict([(' '.join([h[0] for h in d]), d) for d in data])
 
-save_pos(message_train[:1000], 'message_train_pos.txt')
-save_pos(message_test[:1000], 'message_test_pos.txt')
-
-read_pos('message_train_pos.txt')
+#save_pos(message_train[:10], 'xx.txt')
+#save_pos(message_test[:1000], 'message_test_pos.txt')
+#read_pos('txt.txt')
